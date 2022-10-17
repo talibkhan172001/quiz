040419fed42f5e655fa0938085fdac8413d6086a
talib <talibkhan172001.com>
2022-10-17 21:50:23 +0530

    quiz app:

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ❤️ by Talib Khan
+    </div>
+  );
+};
+
+export default Footer;
